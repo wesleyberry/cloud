022be7e833ebd4a515602212d68d24c5653f211a
Wesley Berry <wesleyberry52@gmail.com>
2020-02-12 15:39:12 -0600

    added review

--- composition.js
+++ composition.js
@@ -1,24 +1,44 @@
-const barker = state => ({
-    bark: () => console.log(`Woof, I am ${state.name}`)
-});
+// const barker = state => ({
+//     bark: () => console.log(`Woof, I am ${state.name}`)
+// });
 
-const driver = state => ({
-    drive: () => state.position = state.position + state.speeed
-});
+// const driver = state => ({
+//     drive: () => state.position = state.position + state.speeed
+// });
 
-barker({ name: 'Karo' }).bark();
+// barker({ name: 'Karo' }).bark();
 
-const murderRobotDog = name => {
-    let state = {
-        name,
-        speed: 100,
-        position: 0
-    }
-    return Object.assign(
-        {},
-        barker(state),
-        driver(state)
-    )
-}
+// const murderRobotDog = name => {
+//     let state = {
+//         name,
+//         speed: 100,
+//         position: 0
+//     }
+//     return Object.assign(
+//         {},
+//         barker(state),
+//         driver(state)
+//     )
+// }
 
-murderRobotDog('sniffles').bark();
+// murderRobotDog('sniffles').bark();
+// ---------------------------------------------
+// function Dog (name, energy, breed) {
+//     let dog = {
+//         name,
+//         energy,
+//         breed
+//     }
+
+//     return Object.assign(
+//         dog,
+//         eater(dog),
+//         sleeper(dog),
+//         player(dog),
+//         barker(dog)
+//     )
+// }
+
+// const leo = Dog('Leo', 10, 'Goldendoodle');
+// leo.eat(10);
+// leo.bark();
